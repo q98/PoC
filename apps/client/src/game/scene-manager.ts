eaebacda80667689569b22b01bbddbd70352e28d
NumlockDev <hiraethonline@gmail.com>
2020-09-18 05:07:37 -0400

    need to scale back certain things and probably remove angular, typescript doesn't like babylon materials atm?

--- apps/client/src/game/scene-manager.ts
+++ apps/client/src/game/scene-manager.ts
@@ -1,4 +1,5 @@
-import { Engine, Scene, UniversalCamera, Vector3, HemisphericLight } from '@babylonjs/core';
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, Color3 } from '@babylonjs/core';
+import { GridMaterial } from 'babylonjs-materials';
 
 let canvas: HTMLCanvasElement;
 
@@ -6,7 +7,7 @@ let engine: Engine;
 
 let scene: Scene;
 
-let camera: UniversalCamera;
+let camera: ArcRotateCamera;
 
 let hemisphericLight: HemisphericLight;
 
@@ -16,6 +17,8 @@ let lastTime: number;
 
 let isPaused = false;
 
+let gridmaterial: GridMaterial;
+
 const init = (container: HTMLElement) => {
   canvas = document.createElement('canvas');
   container.appendChild(canvas);
@@ -23,7 +26,19 @@ const init = (container: HTMLElement) => {
   engine = new Engine(canvas);
   scene = new Scene(engine);
   hemisphericLight = new HemisphericLight('light', new Vector3(-1, 0, 1), scene);
-  camera = new UniversalCamera('camera', Vector3.Zero(), scene);
+camera = new ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 4, 3, new Vector3(0, 1, 0), scene);
+        camera.attachControl(canvas, true);
+
+
+        let gridMesh = Mesh.CreateGround("grid", 1000.0, 1000.0, 1000, scene);
+        
+
+
+        gridmaterial = new GridMaterial("GridMaterial", scene);
+        gridmaterial.mainColor = new Color3(1, 1, 1);
+        gridmaterial.lineColor = new Color3(1.0, 1.0, 1.0);
+
+        gridMesh.material = gridmaterial;
 
   return scene;
 };
